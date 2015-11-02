d29ce72f6d61f24817018b53c6713b8b73320035
James Kendrick <J.Kendrick@CERN.ch>
2015-11-02 18:48:10 +0000

    Completed CMake slides & homework, comments and readme work still needed

---
+++ MPAGSCipher/TransformChar.cpp
@@ -0,0 +1,59 @@
+#include "TransformChar.hpp"
+
+std::string transformChar(char inputChar){
+  std::string answer{""};
+  
+  if (isdigit(inputChar)){
+    switch (inputChar){
+      case '0':
+	answer="ZERO";
+	break;
+      case '1':
+	answer="ONE";
+	break;
+      case '2':
+	answer="TWO";
+	break;
+      case '3':
+	answer="THREE";
+	break;
+      case '4':
+	answer="FOUR";
+	break;
+      case '5':
+	answer="FIVE";
+	break;
+      case '6':
+	answer="SIX";
+	break;
+      case '7':
+	answer="SEVEN";
+	break;
+      case '8':
+	answer="EIGHT";
+	break;
+      case '9':
+	answer="NINE";
+	break;
+      }
+    }
+    // If not number, check if character
+  else if (isalpha(inputChar)){
+    //std::cout<<"Passed alpha test"<<std::endl;
+    if (islower(inputChar)){
+	//std::cout<<"Is lower case"<<std::endl;
+      char tempchar = toupper(inputChar);
+      answer = tempchar;
+      }
+    else {
+      //std::cout<<"Is upper case"<<std::endl;
+    answer = inputChar;
+    }
+      //If lower case character
+      //Convert to upper case
+      // Add character to string
+  }
+    //std::cout<<"End of funciton"<<std::endl;
+  return answer;
+    // Other wise, do nothing
+}
--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
-std::string transformChar(const char inputChar);
+#include "TransformChar.hpp"
+
+
 bool processCommandLine(const int& argc, char* argv[], bool& wantHelp, bool& wantO, bool& wantI, bool& wantV, std::string& iValue, std::string& oValue);
 
 
@@ -73,63 +75,7 @@ int main(int argc, char* argv[]){
 }
 
 
-std::string transformChar(char inputChar){
-  std::string answer{""};
-  
-  if (isdigit(inputChar)){
-    switch (inputChar){
-      case '0':
-	answer="ZERO";
-	break;
-      case '1':
-	answer="ONE";
-	break;
-      case '2':
-	answer="TWO";
-	break;
-      case '3':
-	answer="THREE";
-	break;
-      case '4':
-	answer="FOUR";
-	break;
-      case '5':
-	answer="FIVE";
-	break;
-      case '6':
-	answer="SIX";
-	break;
-      case '7':
-	answer="SEVEN";
-	break;
-      case '8':
-	answer="EIGHT";
-	break;
-      case '9':
-	answer="NINE";
-	break;
-      }
-    }
-    // If not number, check if character
-  else if (isalpha(inputChar)){
-    //std::cout<<"Passed alpha test"<<std::endl;
-    if (islower(inputChar)){
-	//std::cout<<"Is lower case"<<std::endl;
-      char tempchar = toupper(inputChar);
-      answer = tempchar;
-      }
-    else {
-      //std::cout<<"Is upper case"<<std::endl;
-    answer = inputChar;
-    }
-      //If lower case character
-      //Convert to upper case
-      // Add character to string
-  }
-    //std::cout<<"End of funciton"<<std::endl;
-  return answer;
-    // Other wise, do nothing
-}
+
 
 
 bool processCommandLine(const int& argc, char* argv[], bool& wantHelp, bool& wantO, bool& wantI, bool& wantV, std::string& iValue, std::string& oValue){
