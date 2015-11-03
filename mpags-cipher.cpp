aa966957aff11ea988e18e8ad6cf305a63a1f6bc
James Kendrick <J.Kendrick@CERN.ch>
2015-11-03 12:09:03 +0000

    Completed lecplating of IO. Pr, compiles but not tested

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -4,7 +4,14 @@
 
 
 bool processCommandLine(const int& argc, char* argv[], bool& wantHelp, bool& wantO, bool& wantI, bool& wantV, std::string& iValue, std::string& oValue);
+void printHelp();
+void printVersion(float vNumber);
 
+bool readStream(std::string& savedString);
+bool readStream(std::string& savedString, std::string inPutFileName);
+
+bool outStream(std::string& savedString);
+bool outStream(std::string& savedString, std::string outPutFileName);
 
 int main(int argc, char* argv[]){
   float vNumber = 0.10;
@@ -19,58 +26,53 @@ int main(int argc, char* argv[]){
     return 1;
     }
   if (wantHelp){
-    std::cout<<"HELP"<<"\n";
+    printHelp();
     return 0;
     }
-  if (wantO){
+  if (wantV){
+    printVersion(vNumber);
+    return 0;
+    }
+
+// Commented unneed cout sections
+/*   *  if (wantO){
     std::cout<<"Output file is: "<<oValue<<"\n";
     }
   if (wantI){
     std::cout<<"Input file is: "<<iValue<<"\n";
     }
-  if (wantV){
-    std::cout<<"Version number is: "<<vNumber<<"\n";
-    return 0;
-    }
-
-    
+*/    
 
 
   
   
   std::string bigString{""};
-  
+  bool inputSuccess{false};
   if (wantI){
-    std::ifstream in_file {iValue};
-    bool ok_to_read = in_file.good();
-    if (ok_to_read==false){
-      std::cout<<"Input file not OK to read \n";
-      return 1;
+    inputSuccess=readStream (bigString, iValue);
     }
-    char inputChar{};
-    while (in_file>>inputChar) {
-      bigString = bigString+transformChar(inputChar);   
-    }    
-  }
-  else {
-    char inputChar {};
-    while (std::cin>>inputChar)  {
-      bigString = bigString + transformChar(inputChar);
+  else{
+    inputSuccess=readStream (bigString);
     }
-  }
-  
+
+  if (inputSuccess == false){
+    std::cout<<"Input failed \n";
+    return 1;
+    }
+ 
+  bool outputSuccess{false}; 
   if (wantO){
-    std::ofstream out_file {oValue};
-    bool ok_to_write = out_file.good();
-    if (ok_to_write==false){
-      std::cout<<"Output file not OK to write \n";
-      return 1;
-      }
-    out_file<<bigString<<"\n";    
+    outputSuccess = outStream (bigString, oValue);
     }
   else {
-    std::cout<<bigString<<"\n";
-  }
+    outputSuccess = outStream (bigString);
+    }
+  
+  if (outputSuccess == false)
+    {
+    std::cout<<"Output failed \n";
+    return 1;
+    }
  //system("eject");
 }
 
@@ -107,3 +109,59 @@ bool processCommandLine(const int& argc, char* argv[], bool& wantHelp, bool& wan
     
     return true;
   }
+
+void printHelp(){  
+    std::cout<<"HELP:"<<"\n";
+    std::cout<<"Please run mpags-cipher. Additional command line arguements include: \n";
+    std::cout<<"--version for verion number \n";
+    std::cout<<"-i for file input, followed by the input file name \n";
+    std::cout<<"-o for file output, followed by the output file name \n";
+    std::cout<<"Defaults allow for command line entry & return of results";
+   }
+
+void printVersion (float vNumber){
+    std::cout<<"Version number is: "<<vNumber<<"\n";
+  }
+
+bool readStream(std::string& savedString){
+    char inputChar {};
+    while (std::cin>>inputChar)  {
+      savedString = savedString + transformChar(inputChar);
+    }
+    return true;
+    }
+
+bool readStream(std::string& savedString, std::string inPutFileName){
+    std::ifstream in_file {inPutFileName};
+    bool ok_to_read = in_file.good();
+    if (ok_to_read==false){
+      std::cout<<"Input file not OK to read \n";
+      return false;
+    }
+    char inputChar{};
+    while (in_file>>inputChar) {
+      savedString = savedString+transformChar(inputChar);   
+    }
+    return true;    
+  }
+
+
+bool outStream(std::string& savedString){
+  std::cout<<savedString<<"\n";
+  }
+
+bool outStream(std::string& savedString, std::string outPutFileName){
+  std::ofstream out_file {outPutFileName};
+  bool ok_to_write = out_file.good();
+  if (ok_to_write==false){
+    std::cout<<"Output file not OK to write \n";
+    return false;
+    }
+  else {
+    out_file<<savedString<<"\n";    
+    return true;
+    }
+  }
+
+  
+
